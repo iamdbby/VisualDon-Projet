f98dd0cb4420b90cfb14ee06c2107c6bdc41626a
Deby <deborah.mayamba@gmail.com>
2022-03-18 23:00:19 +0100

    wireframe

---
+++ Projet/src/index.js
@@ -0,0 +1,12 @@
+import data1 from "../data/Demande-suisse-etrangere-l'hebergement-touristique.csv"
+import data2 from "../data/Evolution-demande-continent-principaux-pays-dans l'hôtellerie.csv"
+import data3 from "../data/Part-nuitées-continent-pays-provenance.csv"
+import data4 from "../data/Taux-net-occupation-chambres.csv"
+import data5 from "../data/Ventilation-mensuelle-nuitées dans-hébergement.csv"
+
+
+console.log(data1);
+console.log(data2);
+console.log(data3);
+console.log(data4);
+console.log(data5);
